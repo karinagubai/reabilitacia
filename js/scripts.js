7504ae98571ab7b2bb4c848abff469e5aeaea3b9
Карина Губайдуллина <karina@MacBook-Air-Karina.local>
2019-08-21 13:42:52 +0500

    add other pages

--- js/scripts.js
+++ js/scripts.js
@@ -26,3 +26,42 @@ var init = new Switchery(elem, { disabled: true });
 $(document).ready(function(){
   $('input[type="tel"]').inputmask("+7 (999) 999-99-99");
 });
+
+
+// Filter aspects
+
+// init Isotope
+$('.rew-patients').hide();
+var $grid = $('.grid').isotope({
+  // options
+});
+// filter items on button click
+$('.filter-nav').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  if ( filterValue == '.rew-patients' ) {
+    $('.rew-patients').show();
+  }
+  $grid.isotope({ filter: filterValue });
+});
+
+// Filter mediaGallery
+
+$('.btn-gallery').on('click', function(){
+  const filterValue = $(this).attr('data-filter');
+  $('.gallery-item').each(function(index, item){
+    if($(item).hasClass(filterValue)){
+      $(item).show();
+    } else {
+      $(item).hide();
+    }
+  });
+});
+
+
+// Active button in filter-nav
+
+$('.btn-filter').on('click', function(){
+    $('.btn-filter').removeClass('btn-active');
+    $(this).addClass('btn-active');
+
+});
